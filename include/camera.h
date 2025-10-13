b9d808173af9c91d794806395b5724e63f4e5d16
Andrey Naboka <and.naboka@yandex.ru>
2025-10-13 19:04:28 +0300

    add method for set mouse sensitivity

--- include/camera.h
+++ include/camera.h
@@ -8,6 +8,7 @@ class camera
 public:
    camera();
    void set_speed(const float new_speed) { _speed = new_speed; }
+   void set_mouse_sensitivity(const float mouse_sensitivity) { _mouse_sensitivity = mouse_sensitivity; }
    void move_left();
    void move_right();
    void move_forward();
@@ -17,7 +18,8 @@ public:
    void mouse_scroll(const double x_offset, const double y_offset);
 
 private:
-   float _speed = 1.0f;
+   float _speed = 3.0f;
+   float _mouse_sensitivity = 0.25f;
    glm::mat4 _proj_matrix;
    glm::vec3 _pos;
    glm::vec3 _front;
