3d0aaecbfb7c5e52c33f98e391a1d1c3ced0ccf6
Andrey Naboka <and.naboka@yandex.ru>
2026-04-29 23:06:52 +0300

    Add RenderObject class

---
+++ src/RenderObject.cpp
@@ -0,0 +1,32 @@
+#include "RenderObject.h"
+#include <glad/gl.h>
+#include <iostream>
+
+RenderObject::RenderObject(const std::vector<float> &vertices,
+                           const std::vector<unsigned int> &indices) {
+  glGenVertexArrays(1, &_VAO);
+  glGenBuffers(1, &_VBO);
+  glGenBuffers(1, &_EBO);
+  glBindVertexArray(_VAO);
+  glBindBuffer(GL_ARRAY_BUFFER, _VBO);
+  glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float),
+               vertices.data(), GL_STATIC_DRAW);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, _EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, indices.size() * sizeof(unsigned int),
+               indices.data(), GL_STATIC_DRAW);
+  // Layout: 3pos, 3norm, 2uv = 8 floats per vertex
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                        (void *)(3 * sizeof(float)));
+  glEnableVertexAttribArray(1);
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float),
+                        (void *)(6 * sizeof(float)));
+  glEnableVertexAttribArray(2);
+  _indexCount = indices.size();
+}
+
+void RenderObject::Draw() {
+  glBindVertexArray(_VAO);
+  glDrawElements(GL_TRIANGLES, _indexCount, GL_UNSIGNED_INT, 0);
+}
