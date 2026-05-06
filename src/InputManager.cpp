61a09940fd7cd7287bc4d41017c591aaa6233526
Andrey Naboka <and.naboka@yandex.ru>
2026-05-06 17:01:21 +0300

    Implement Init, Update, IsKeyPressed methods in InputManager. WIP

--- src/InputManager.cpp
+++ src/InputManager.cpp
@@ -1,10 +1,25 @@
 #include "InputManager.h"
+#include "Logger.h"
+#include <GLFW/glfw3.h>
 
-void InputManager::Init(GLFWwindow *wnd) {}
+void InputManager::Init(GLFWwindow *wnd) {
+  if (!wnd) {
+    log("[InputManager] Init. GLFWwindow must be not null");
+    return;
+  }
+  _wnd = wnd;
+  glfwSetWindowUserPointer(_wnd, this);
+  glfwSetCursorPosCallback(_wnd, MouseCallback);
+}
 
-void InputManager::Update() {}
+void InputManager::Update() {
+	_mouseDeltaX=0.0f;
+	_mouseDeltaY=0.0f;
+}
 
-bool InputManager::IsKeyPressed(const int key) const { return false; }
+bool InputManager::IsKeyPressed(const int key) const { 
+	return glfwGetKey(_wnd, key);
+}
 
 void InputManager::GetMouseDelta(float &x, float &y) const {}
 
