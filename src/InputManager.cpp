f48a43b1c8543096a63de30e90c899e4d2be6f52
Andrey Naboka <and.naboka@yandex.ru>
2026-05-07 18:31:47 +0300

    Impelement InputManager class. WIP

--- src/InputManager.cpp
+++ src/InputManager.cpp
@@ -9,18 +9,31 @@ void InputManager::Init(GLFWwindow *wnd) {
   }
   _wnd = wnd;
   glfwSetWindowUserPointer(_wnd, this);
+  glfwGetCursorPos(_wnd, &_lastMouseX, &_lastMouseY);
   glfwSetCursorPosCallback(_wnd, MouseCallback);
 }
 
 void InputManager::Update() {
-	_mouseDeltaX=0.0f;
-	_mouseDeltaY=0.0f;
+  _mouseDeltaX = 0.0f;
+  _mouseDeltaY = 0.0f;
 }
 
-bool InputManager::IsKeyPressed(const int key) const { 
-	return glfwGetKey(_wnd, key);
+bool InputManager::IsKeyPressed(const int key) const {
+  return glfwGetKey(_wnd, key);
 }
 
-void InputManager::GetMouseDelta(float &x, float &y) const {}
+void InputManager::GetMouseDelta(float &x, float &y) const {
+  x = _mouseDeltaX;
+  y = _mouseDeltaY;
+}
 
-void InputManager::MouseCallback(GLFWwindow *w, double xPos, double yPos) {}
+void InputManager::MouseCallback(GLFWwindow *w, double xPos, double yPos) {
+  auto *input = reinterpret_cast<InputManager *>(glfwGetWindowUserPointer(w));
+  if (!input)
+    return;
+
+  input->_mouseDeltaX = static_cast<float>(xPos - input->_lastMouseX);
+  input->_mouseDeltaY = static_cast<float>(input->_lastMouseY - yPos);
+  input->_lastMouseX = xPos;
+  input->_lastMouseY = yPos;
+}
