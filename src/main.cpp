7659a6ddf02b59f7cf24b45d4fbc703d7b72b167
Andrey Naboka <and.naboka@gmail.com>
2026-05-10 01:45:43 +0300

    Create Window and add main loop. Update gitignore file

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,18 @@
 #include "Window.h"
+#include "InputManager.h"
 #include <string>
 
 int main() {
   Window wnd(600, 400, std::string("World"));
+  InputManager input;
+  input.Init(wnd.GetNativeHanle());
+  wnd.PollEvents();
+
+  while (!wnd.ShouldClose()) {
+    input.Update();
+    wnd.SwapBuffers();
+    wnd.PollEvents();
+  }
 
   return 0;
 }
