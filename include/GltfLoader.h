36c2f95bc80a5c8796cf25b1994a309713333ae9
Andrey Naboka <and.naboka@gmail.com>
2026-05-17 21:12:40 +0300

    Skeleton prepare. WIP

--- include/GltfLoader.h
+++ include/GltfLoader.h
@@ -11,7 +11,7 @@ struct GltfModelData {
   std::shared_ptr<Mesh> mesh;
   std::vector<std::shared_ptr<Animation>> animations;
   std::vector<glm::mat4> inverseBindMatrices;
-  std::unordered_map<std::string, size_t> boneToIndex;
+  std::unordered_map<std::string, size_t> boneNameToIndex;
   bool isSkinned = false;
 };
 
--- src/GltfLoader.cpp
+++ src/GltfLoader.cpp
@@ -88,6 +88,26 @@ GltfModelData GltfLoader::Load(const std::string &assetPath) {
       vertices[i].uv = {temp[i * 2], temp[i * 2 + 1]};
   }
 
+  if (!isSkinned && activeSkin) {
+    result.inverseBindMatrices.resize(activeSkin->joints_count);
+    // read inverse bind matrices
+    if (activeSkin->inverse_bind_matrices) {
+      cgltf_accessor_read_float(
+          activeSkin->inverse_bind_matrices, 0,
+          reinterpret_cast<float *>(result.inverseBindMatrices.data()),
+          activeSkin->joints_count * 16);
+    } else {
+      for (auto &m : result.inverseBindMatrices)
+        m = glm::mat4(1.0f);
+    }
+    for (size_t i = 0; i < activeSkin->joints_count; ++i) {
+      const cgltf_node *jointNode = activeSkin->joints[i];
+      const std::string name =
+          jointNode->name ? jointNode->name : ("joint_" + std::to_string(i));
+      result.boneNameToIndex[name] = i;
+    }
+  }
+
   //--------------------------------------
   LogInfo("[GltfLoader] load success");
   cgltf_free(data);
