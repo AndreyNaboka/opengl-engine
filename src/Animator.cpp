95fd547581533da80094800862eec313cd6125e4
Andrey Naboka <and.naboka@gmail.com>
2026-05-19 23:19:00 +0300

    Add SetAnimation and Update methods in Animator class. WIP

--- src/Animator.cpp
+++ src/Animator.cpp
@@ -1,10 +1,25 @@
 #include "Animator.h"
+#include <cmath>
 
 void Animator::SetAnimation(const Animation *anim,
                             const std::vector<glm::mat4> &inverseBindMatrices,
-                            bool loop) {}
+                            bool loop) {
+  _currentAnim = anim;
+  _boneOffsets = inverseBindMatrices;
+  _loop = loop;
+  _time = 0.0f;
+  if (anim)
+    _finalBoneMatrices.resize(anim->boneIndexMap.size(), glm::mat4(1.0f));
+}
 
-void Animator::Update(float dt) {}
+void Animator::Update(float dt) {
+  if (!_currentAnim)
+    return;
+  _time += dt;
+  if (_loop && _time > _currentAnim->duration)
+    _time = std::fmod(_time, _currentAnim->duration);
+  CalculateBoneTransforms(0, glm::mat4(1.0f), _time);
+}
 
 void Animator::CalculateBoneTransforms(size_t boneIndex,
                                        const glm::mat4 &parentTransform,
